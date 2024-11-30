4a2524f9b4a5a85d34c515975bae2e286f0a1042
Vaishnavishete123 <130748998+Vaishnavishete123@users.noreply.github.com>
2024-11-30 19:07:44 +0530

    first commit

---
+++ Personal_Portfolio/script.js
@@ -0,0 +1,8 @@
+// Optional interactivity can go here
+console.log("Welcome to my portfolio!");
+
+function toggleNavbar() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');  // Toggle 'active' class to show/hide the
+}
+
